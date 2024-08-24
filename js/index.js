ca26134bd2121caface73751fb732e88bc13890a
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-08-24 13:44:42 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -24,3 +24,36 @@ for(let i = 0; i < skills.length; i++){
     titleItem.textContent = skills[i]; 
     ulItem.append(titleItem);
 }
+
+// Handle Message Form Submit
+const messageForm = document.getElementById("leave_message");
+messageForm.addEventListener("submit", 
+    event => {
+        
+        event.preventDefault();
+
+        let name = event.target.usersName.value;
+        let email = event.target.usersEmail.value;
+        let message = event.target.usersMessage.value;
+        console.log(`${name} ${email} ${message}`);
+        event.target.reset();
+
+        // Display Messages in List
+        const messageSection = document.getElementById("messages");
+        let messageList = messageSection.querySelector("ul");
+        messageList.style.listStyle = 'none';
+        let newMessage = document.createElement("li");
+
+        let removeButton = document.createElement("button");
+        removeButton.innerHTML = "<img class = 'btn-remove' src = 'img/git status.png' alt = 'remove icon'>";
+        removeButton.type = "button";
+        removeButton.addEventListener("click", 
+            event => {
+            let entry = event.target.parentNode;
+            entry.remove();
+        );
+        
+        newMessage.innerHTML = `<a href='mailto:${email}'><strong>${name}</strong></a>:  <span> ${message} </span> `;
+        newMessage.appendChild(removeButton);
+        messageList.append(newMessage);
+    });
